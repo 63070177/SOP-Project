930ac5a00e8cfee62f596c12debfb3e3472933eb
63070177 <73645471+63070177@users.noreply.github.com>
2022-12-20 14:36:27 +0700

    Update subjectTeacher.vue

--- src/views/subjectTeacher.vue
+++ src/views/subjectTeacher.vue
@@ -1,5 +1,5 @@
 <template>
-            <div style="color: #fff;">
+    <div style="color: #fff;">
         <div class="columns">
             <div class="column">
                 <div class="sidebar" style="padding: 20px;">
@@ -51,120 +51,85 @@
                     <h2>รายวิชาที่สอน</h2>
                 </div>
                 <div class="column">
-                    <router-link :to="{path: `modifySubject`}">
-                    <button type="modify" class="button is-primary is-outlined" >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-plus-lg" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd"
-                                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
-                        </svg>
-                        เพิ่มรายวิชา
-                    </button></router-link>
+                    <router-link :to="{ path: `modifySubject` }">
+                        <button type="modify" class="button is-primary is-outlined">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                            </svg>
+                            เพิ่มรายวิชา
+                        </button></router-link>
                 </div>
                 <div class="block">
                     <table style="width:80%; color: #fff;">
-                            <tr>
-                                <th class="is-size-5" style="color: #fff;"></th>
-                                <th class="is-size-5" style="color: #fff;">รหัสวิชา</th>
-                                <th class="is-size-5" style="color: #fff;">ชื่อวิชา</th>
-                                <th class="is-size-5" style="color: #fff;">วัน-เวลา</th>
-                                <th class="is-size-5" style="color: #fff;">รับ</th>
-                                <th class="is-size-5" style="color: #fff;">ลง</th>
-                            </tr>
-                            <tr>
-                                <td>
+                        <tr>
+                            <th class="is-size-5" style="color: #fff;"></th>
+                            <th class="is-size-5" style="color: #fff;">ชื่อวิชา</th>
+                            <th class="is-size-5" style="color: #fff;">วัน-เวลา</th>
+                            <th class="is-size-5" style="color: #fff;">ครูผู้สอน</th>
+                        </tr>
+                        <tr v-if="!showForm">
+                            <td>
+                                    <button type="modify" class="button is-primary is-outlined"
+                                        style="margin-right: 15px;" @click="showForm = true;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                            <path
+                                                d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                                        </svg>
+                                        แก้ไข
+                                    </button>
+                                    <button type="modify" class="button is-danger is-outlined">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                            <path
+                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                        </svg>
+                                        ลบ
+                                    </button>
+                            </td>
+                            <td>SOP</td>
+                            <td>วันจันทร์ 14.00-16.00 น.</td>
+                            <td>...</td>
+                        </tr>
+                        <tr v-if="showForm">
+                            <td>
+                                <div class="columns">
                                     <div class="column">
-                                        <button type="modify" class="button is-primary is-outlined" style="margin-right: 15px;">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
-                                            </svg>
-                                            แก้ไข
-                                        </button>
-                                        <button type="modify" class="button is-danger is-outlined">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-                                            </svg>
-                                            ลบ
-                                        </button>
+                                        <button class="button is-primary is-outlined" v-if="showForm">ตกลง</button>
+
                                     </div>
-                                </td>
-                                <td>06016323</td>
-                                <td>SOP</td>
-                                <td>วันจันทร์ 14.00-16.00 น.</td>
-                                <td>50</td>
-                                <td>48</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="column">
-                                        <button type="modify" class="button is-primary is-outlined" style="margin-right: 15px;">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
-                                            </svg>
-                                            แก้ไข
-                                        </button>
-                                        <button type="modify" class="button is-danger is-outlined">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-                                            </svg>
-                                            ลบ
-                                        </button>
+
+                                    <div class="column is-12">
+                                        <button class="button is-danger is-outlined" v-if="showForm"
+                                            @click="showForm = false;">ยกเลิก</button>
                                     </div>
-                                </td>
-                                <td>06016324</td>
-                                <td>OOP</td>
-                                <td>วันพุธ 14.00-16.00 น.</td>
-                                <td>80</td>
-                                <td>5</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="column">
-                                        <button type="modify" class="button is-primary is-outlined" style="margin-right: 15px;">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
-                                            </svg>
-                                            แก้ไข
-                                        </button>
-                                        <button type="modify" class="button is-danger is-outlined">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-                                            </svg>
-                                            ลบ
-                                        </button>
-                                    </div>
-                                </td>
-                                <td>06016325</td>
-                                <td>MIS</td>
-                                <td>วันอังคาร 14.00-16.00 น.</td>
-                                <td>20</td>
-                                <td>16</td>
-                            </tr>
+                                </div>
+                            </td>
+                            <td>
+                                <input class="input is-small" type="text" style="width:80%;">
+                            </td>
+                            <td>
+                                <input class="input is-small" type="text" style="width:80%;">
+                            </td>
+                            <td>
+                                <input class="input is-small" type="text" style="width:80%;">
+                            </td>
+                        </tr>
                     </table>
                 </div>
             </div>
-    </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
+export default {
     name: "subjectTeacher",
     data() {
         return {
-
+            showForm: false,
         }
     },
     methods: {
